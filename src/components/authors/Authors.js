883b090570ec8fc0deb58ddc1e1979f428d879e3
BT <bahartofigh@gmail.com>
2023-03-29 20:54:44 +0430

    add slug and styles

--- src/components/authors/Authors.js
+++ src/components/authors/Authors.js
@@ -3,6 +3,7 @@ import { useQuery } from "@apollo/client";
 import { GET_AUTHORS_INFO } from "../../graphql/queries";
 import { Container, Grid } from "@mui/material";
 import { Typography , CardActionArea , Card , CardContent , CardMedia } from "@mui/material";
+import { Link } from "react-router-dom";
 
 
 import Loader from "../shared/Loader";
@@ -13,20 +14,21 @@ const Authors = () => {
 
   if (errors) return <h3>Error...</h3>;
 
-  
 
   return (
-    <Container maxWidth="lg"> 
+    <Container maxWidth="lg" style={{ height: "1000px"}}> 
       <Grid container spacing={2} padding={3}>
       {data.authors.map((author) => (
-        <Grid item xs={12} md={3} mt={4} key={author.id}>
-          <Card sx={{ maxWidth: 345 , height: "320px" , boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: 4 }}>
+        <Grid item xs={12} sm={6} md={3} mt={4} key={author.id}>
+          <Card sx={{ maxWidth: 345 , height: "300px" , boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: 4 }}>
     <CardActionArea>
       <CardMedia component="img" height="200" width="200" image={author.avatar.url} alt="profile"/>
       <CardContent>
-        <Typography gutterBottom variant="h5" component="h4">
+      <Link to={`/authors/${author.slug}`} style={{ textDecoration: "none"}} >
+        <Typography gutterBottom variant="h5" component="div">
         {author.name}
         </Typography>
+      </Link>
       </CardContent>
     </CardActionArea>
   </Card>
